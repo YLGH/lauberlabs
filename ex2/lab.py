1a6e03099d5dc5ef8467a6cc5f36d4e199c1a757
YLGH <y4ngyin@gmail.com>
2017-03-14 18:23:58 -0400

    asdf

--- ex2/lab.py
+++ ex2/lab.py
@@ -10,7 +10,6 @@ g = 9.81
 l = g
 q = 0
 F = 0
-(theta_0, omega_0) = (pi/2, 0.0)
 
 
 def derivatives(y,t, omega_d):
@@ -19,7 +18,7 @@ def derivatives(y,t, omega_d):
 num_oscillations = 10
 t = np.linspace(0.0, num_oscillations*2*pi, sqrt(num_oscillations)*num_oscillations*2*pi)
 
-y0 = [pi/2, 0.0]
+y0 = [0.01, 0.0]
 omega_d = 2/3
 y = scipy.integrate.odeint(derivatives, y0, t, args=(omega_d,))
 
@@ -30,11 +29,12 @@ for i in range(len(y)):
 	theta = y[i,0]
 	omega = y[i,1]
 	energy.append(m*l*l*(1-cos(theta) + omega*omega/2.0))
-print(energy)
+
+
 fig, (ax0, ax1) = plt.subplots(nrows=2, sharex=False)
 
 ax0.plot(t, thetas[:,0], '-')
-ax0.set_title(str(num_oscillations) + " oscillation + theta_0:" + str(round(theta_0,2)) + " omega_0: " +str(omega_0))
+ax0.set_title(str(num_oscillations) + " oscillation + theta_0:" + str(round(y0[0],2)) + " omega_0: " +str(y0[1]))
 
 ax1.plot(t, energy, '-')
 ax1.set_title('Energy vs time')
