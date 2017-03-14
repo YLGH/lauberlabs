f132fae5599dd9f2e4314c62dc63cd24c34e0678
YLGH <y4ngyin@gmail.com>
2017-03-14 19:41:55 -0400

    asdf

--- ex1/lab.py
+++ ex1/lab.py
@@ -37,34 +37,47 @@ def monte_carlo_mean_error(f, num_samples, nt):
     for v in values:
         ms += ((v-mean)**2.0)
     rms = sqrt(ms/nt)
-    
-    return (mean, rms)
+     
+    theory_error = estimate_integral(f, num_samples)[1]   
+    return (mean, rms, theory_error)
 
         
-N_to_plot = [10, 50, 100, 500, 1000, 5000, 10000, 50000]
-
+N_to_plot = [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000]
 
 # example data
 x = []
 y = []
 yerror = []
 
+error_ratio = []
+
 for n in N_to_plot:
-    (mean, error) = monte_carlo_mean_error(lambda y: 10**6*vecsin(y), n, 25)
+    (mean, error, theory_error) = monte_carlo_mean_error(lambda y: 10**6*vecsin(y), n, 25)
+    error_ratio.append(error/theory_error)
+    print(n, error, theory_error)
     x.append(n)
     y.append(mean)
     yerror.append(error)
 
 
 
-fig, (ax0, ax1) = plt.subplots(nrows=2, sharex=True)
-ax0.errorbar(x, y, yerr=yerror, fmt='-o')
-ax0.set_title('Monte Carlo Samples')
-ax0.set_xscale('log')
+# fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, sharex=False)
 
-ax1.scatter(x, yerror)
-ax1.set_title('Error vs Samples')
-ax1.set_yscale('log')
-ax1.set_xscale('log')
+fig, (ax2) = plt.subplots(nrows=1, sharex=False)
+# ax0.errorbar(x, y, yerr=yerror, fmt='-o')
+# ax0.set_title('Monte Carlo Samples')
+# ax0.set_xscale('log')
+
+# ax1.scatter(x, yerror)
+# ax1.set_title('Error vs Samples')
+# ax1.set_yscale('log')
+# ax1.set_xscale('log')
+
+ax2.plot(x, error_ratio)
+ax2.set_title('Error ratio (rms/sigma) vs x')
+ax2.set_yscale('log')
+ax2.set_xscale('log')
+
+
 
 plt.show()
